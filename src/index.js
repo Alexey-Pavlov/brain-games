ae372659e57634dff824b72d10ddbea1116afdf8
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2020-02-16 20:41:26 +0300

    connect readline-sync library, made a welcome function

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export const sayHello = () => {
+    console.log('Welcome to the Brain Games!');
+    const actual = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${actual}!`);
+};
+
