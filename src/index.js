a1ebbd40b129c3bd4251e81c0f718a264c5ac130
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2020-02-19 21:06:37 +0300

    Add game "Parity Check"

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ const sayHello = () => {
   console.log('Welcome to the Brain Games!');
   const actual = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${actual}!`);
+  return actual;
 };
 
-export default sayHello;
+const evenGame = () => {
+  const name = sayHello();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * (100 + 1));
+    const isEven = (randomNumber % 2 === 0);
+    const correctAnswer = isEven ? 'yes' : 'no';
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if ((isEven && (answer === 'yes')) || (!isEven && (answer === 'no'))) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${name}!`);
+      return 0;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+  return 0;
+};
+
+export { sayHello, evenGame };
